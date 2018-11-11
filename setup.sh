39c088eb9587886deebd9d78b0707f1dcfb46f34
vienai8d <31370233+vienai8d@users.noreply.github.com>
2018-11-11 22:00:24 +0900

    add dotfiles and setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Setup for vim
+cp vim/vimrc ~/.vimrc
+
+# Setup for git
+cp git/gitconfig ~/.gitconfig
+cp git/gitignore_global ~/.gitignore_global
+
+# Setup for bash
+cp bash/bash_profile ~/.bash_profile
+
